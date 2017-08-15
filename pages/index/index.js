a4b9068a540b7fc6f1f42c92891cabbf844bebb3
cloud.chen <cloud.chen@cdskysoft.com>
2017-08-15 13:44:21 +0800

    [文本][test][cloud.chen]

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,53 @@
-//index.js
-//获取应用实例
-var app = getApp()
+var total_micro_second = 10000 * 1000;
+
+/* 毫秒级倒计时 */
+function count_down(that) {
+  // 渲染倒计时时钟
+  that.setData({
+    clock: date_format(total_micro_second)
+  });
+
+  if (total_micro_second <= 0) {
+    that.setData({
+      clock: "已经截止"
+    });
+    // timeout则跳出递归
+    return;
+  }
+  setTimeout(function () {
+    // 放在最后--
+    total_micro_second -= 10;
+    count_down(that);
+  }
+    , 10)
+}
+
+// 时间格式化输出，如03:25:19 86。每10ms都会调用一次
+function date_format(micro_second) {
+  // 秒数
+  var second = Math.floor(micro_second / 1000);
+  // 小时位
+  var hr = Math.floor(second / 3600);
+  // 分钟位
+  var min = fill_zero_prefix(Math.floor((second - hr * 3600) / 60));
+  // 秒位
+  var sec = fill_zero_prefix((second - hr * 3600 - min * 60));// equal to => var sec = second % 60;
+  // 毫秒位，保留2位
+  var micro_sec = fill_zero_prefix(Math.floor((micro_second % 1000) / 10));
+
+  return hr + ":" + min + ":" + sec + " " + micro_sec;
+}
+
+// 位数不足补零
+function fill_zero_prefix(num) {
+  return num < 10 ? "0" + num : num
+}
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    clock: ''
   },
-  //事件处理函数
   onLoad: function () {
-
+    count_down(this);
   }
-})
+});
