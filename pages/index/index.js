289d14a835c4956b717319b7cb7043c52df85ed4
cloud.chen <cloud.chen@cdskysoft.com>
2017-08-15 14:55:11 +0800

    [封装请求数据的方法wx.request][封装全局数据]

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,22 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var {
+    http
+    } = require('../../utils/util.js');
+//使用对象解构获取全局数据
+var {
+    globalData
+    } = getApp();
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+
   },
   //事件处理函数
   onLoad: function () {
-    wx.request({
-      url: 'https://localhost:63342/jingdong-app/test/test',
-      header: {//请求头
-        "Content-Type": "json"
-      },
-      method: 'GET',
-      success: function(res){
-        console.log(res)
-        console.log(res.data.engineer);
-        console.log(res.data.age);
-      }
-    })
+    var baseUrl = globalData.baseUrl;
+    http(`${baseUrl}test`, (data) => {
+      console.log(data);
+    });
   }
-})
+});
--- utils/util.js
+++ utils/util.js
@@ -1,21 +1,37 @@
 function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
 
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
 
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString();
+    return n[1] ? n : '0' + n
 }
 
-module.exports = {
-  formatTime: formatTime
+//全局共通方法
+function http(url, callback) {
+    //发送请求
+    //wx.request发起的是https请求,一个微信小程序，同时只能有5个网络请求连接
+    wx.request({
+        url,
+        header: {
+            'content-type': 'application/xml'
+        },
+        success: (res)=> {
+            //回调
+            callback(res.data)
+        }
+    })
 }
+module.exports = {
+    formatTime: formatTime,
+    http
+};
