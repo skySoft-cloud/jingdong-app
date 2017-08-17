78f72307ee3c6db1e74f93a46d0a9c66cb092405
James.wang <james.wang@cdskysoft.com>
2017-08-17 11:00:09 +0800

    finish the GUI of this page.

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 //app.js
 App({
-    onLaunch: function() {
-        //调用API从本地缓存中获取数据
-        var logs = wx.getStorageSync('logs') || [];
-        logs.unshift(Date.now());
-        wx.setStorageSync('logs', logs)
-    },
-
-    getUserInfo: function(cb) {
-        var that = this;
-        if (this.globalData.userInfo) {
-            typeof cb == "function" && cb(this.globalData.userInfo)
-        } else {
+    /**
+     * 获取用户信息，供各页面调用
+     */
+    getWechatInfo:function(callback){
+        const that = this;
+        // 1. 若全局userInfo存在，直接调用回调函数
+        if(this.globalData.userInfo){
+            typeof callback == "function" && callback(this.globalData.userInfo);
+        }
+        // 2. 若全局userInfo不存在，用wx.getUserInfo获取userInfo保存到全局
+        else{
             //调用登录接口
-            wx.getUserInfo({
-                withCredentials: false,
-                success: function(res) {
-                    that.globalData.userInfo = res.userInfo;
-                    typeof cb == "function" && cb(that.globalData.userInfo)
+            wx.login({
+                success: function () {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo;
+                            typeof callback == "function" && callback(that.globalData.userInfo);
+                        }
+                    })
                 }
             })
         }
     },
     globalData: {
+        userInfo: null,
         baseUrl: "https://localhost:63342/jingdong-app/test/"
     }
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,16 @@
-//index.js
 //获取应用实例
-var {
+const {
     http
     } = require('../../utils/util.js');
-//使用对象解构获取全局数据
-var {
-    globalData
-    } = getApp();
-
-const app = getApp();
-
 Page({
-    data: {
-        userInfo: {}
-    },
+    data: {},
     //事件处理函数
     onLoad: function () {
-        var baseUrl = globalData.baseUrl;
-        http(`${baseUrl}test`, (data) => {
-            console.log(data);
+        http({
+            url: "test",
+            func: (data) => {
+                console.log(data);
+            }
         });
     }
 });
