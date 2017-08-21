25046b01b913884a0d1659704362df859a8eb1c9
James.wang <james.wang@cdskysoft.com>
2017-08-21 17:58:14 +0800

    format

--- pages/user_info/my_order/my_order.js
+++ pages/user_info/my_order/my_order.js
@@ -1,19 +1,19 @@
 // pages/user_info/my_order/my_order.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    navTabs: ["全部订单", "待付款", "待收货"],
-    activeIndex: 0
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        navTabs: ["全部订单", "待付款", "待收货"],
+        activeIndex: 0
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  },
+    },
     /**
      * nav item的tap事件
      */
