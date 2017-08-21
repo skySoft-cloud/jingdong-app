1496630dcb84b921808c900c0bdc450c36b9200e
James.wang <james.wang@cdskysoft.com>
2017-08-21 17:52:17 +0800

    finished nav bar GUI.

--- pages/user_info/my_order/my_order.js
+++ pages/user_info/my_order/my_order.js
@@ -5,62 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    navTabs: ["全部订单", "待付款", "待收货"],
+    activeIndex: 0
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    /**
+     * nav item的tap事件
+     */
+    tapNavItem: function (e) {
+        this.setData({
+            activeIndex: e.currentTarget.id
+        });
+    }
 })
