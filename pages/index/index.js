3079025d67e9e4fdeca55777ae417bc1125966ab
cloud.chen <cloud.chen@cdskysoft.com>
2017-08-23 14:58:44 +0800

    【user-info制作雪碧图,index页面代码优化】【cloud.chen】

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,14 +3,46 @@ const {
     http
     } = require('../../utils/util.js');
 Page({
-    data: {},
+    data: {
+        num: 0
+    },
     //事件处理函数
     onLoad: function () {
-        http({
-            url: "test",
-            func: (data) => {
-                console.log(data);
-            }
-        });
+        /*请求假数据demo*/
+        /*     http({
+         url: "test",
+         func: (data) => {
+         console.log(data);
+         }
+         });*/
+        this.selectDefaultTimeItem();
+    },
+    /**
+     * 点击链接到订单详情页
+     */
+    tapMyOrder: () => {
+        wx.navigateTo({
+            url: "../user_info/my_order/my_order"
+        })
+    },
+    /*页面初始化加载默认选中进行中的抢购条目*/
+    selectDefaultTimeItem: function () {
+        this.setData({
+            num: 1
+        })
+    },
+    /**
+     * 点击抢购的时间切换样式
+     */
+    clickNum: function (e) {
+        this.setData({
+            num: e.target.dataset.num
+        })
+    },
+    /**
+     * 当searchbar获得焦点时
+     */
+    wxSearchFocus:function(){
+
     }
 });
