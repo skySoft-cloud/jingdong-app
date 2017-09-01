b58c6610da5c91c5fb015af3dbdcc6a6dec5b75c
James.wang <james.wang@cdskysoft.com>
2017-09-01 21:03:29 +0800

    add page '京东券'

--- pages/user_info/received_coupons/received_coupons.js
+++ pages/user_info/received_coupons/received_coupons.js
@@ -1,66 +1,71 @@
 // pages/user_info/received_coupons/received_coupons.js
+const {http} = require('../../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    // 定义头部tab信息
+    navTabs: [],
+    // 定义将activeIndex置为空
+    activeIndex: "",
+    // 定义待使用优惠券信息
+    notUsedCouponInfo: [],
+    // 定义已使用优惠券信息
+    usedCouponInfo: [],
+    // 定义已过期优惠券信息
+    expiredCouponInfo: []
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const that = this;
+    // 调接口GetReceivedCoupon
+    http({
+      url: "GetReceivedCoupon",
+      func: function(data){
+        // 定义渲染到视图层的数组
+        let navTabs = [];
+        // 将返回data重新组合
+        for (let couponArr in data) {
+          // 初始化navTabInfo
+          let navTabInfo = {};
+          // 有优惠券信息，length为信息的长度，没有优惠券信息，将lengtgh置为空
+          let length = data[couponArr].length > 0 ? " "+ data[couponArr].length: "";
+          // 根据couponArr类型做不同字符串拼接
+          if (couponArr == "not_used_coupon") {
+            navTabInfo.navTabName = `待使用${length}`;
+          } else if (couponArr == "expired_coupon"){
+            navTabInfo.navTabName = `已过期${length}`;
+          } else {
+            navTabInfo.navTabName = `已使用${length}`;
+          }
+          //couponInfo[couponArr] = data[couponArr];
+          navTabs.push(navTabInfo);
+        }
+        console.log(navTabs)
+        console.log(data)
+        that.setData({
+          navTabs: navTabs,
+          notUsedCouponInfo: data["not_used_coupon"],
+          usedCouponInfo: data["used_coupon"],
+          expiredCouponInfo: data["expired_coupon"]
+        });
+      }
+    });
+    that.setData({
+      // 此处获取的activeIndex为其他页面传过来的参数用于定位优惠券类型
+      activeIndex: options.activeIndex
+    });
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * nav item的tap事件
    */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  tapNavItem: function (e) {
+    this.setData({
+      activeIndex: e.currentTarget.id
+    });
   }
 })
