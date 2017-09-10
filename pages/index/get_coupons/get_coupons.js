5b85307b95bce6a2e36903136de5a31039447516
james <18302819568@163.com>
2017-09-10 20:20:23 +0800

    finish the page '领取优惠券'

--- pages/index/get_coupons/get_coupons.js
+++ pages/index/get_coupons/get_coupons.js
@@ -1,19 +1,109 @@
-// pages/index/get_coupons/get_coupons.js
+const {http} = require('../../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    couponInfo: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    const that = this;
+    // 调接口GetCoupon
+    http({
+      url: "GetCoupon",
+      func: function(data){
+        that.setData({
+          couponInfo: data
+        });
+      }
+    });
   },
+
+  /**
+   * 点击领取优惠券
+   */
+  tapGetCoupon: function (e) {
+    const that = this,
+        coupon_info = this.data.couponInfo,
+        new_coupon_info = coupon_info,
+        coupon_id = e.currentTarget.id;
+    // 调接口GetCurCoupon
+    http({
+      url: "GetCurCoupon",
+      // 传入当前优惠券的id
+      data: {"coupon_id": coupon_id},
+      func: function(data){
+        // 领取优惠券成功处理
+        if (data.success == "1") {
+          wx.showToast({
+            title: "领取优惠券成功",
+            success: function(res) {
+              // 遍历coupon_info
+              for (let i = 0;i < coupon_info.length; i++){
+                // 找到当前点击的那条数据
+                if (coupon_info[i]["coupon_id"] == coupon_id) {
+                  // 将当前数据的coupon_status置为1
+                  new_coupon_info[i]["coupon_status"] = "1";
+                }
+              }
+              // 重新设置couponInfo数据
+              that.setData({
+                couponInfo: new_coupon_info
+              })
+            }
+          })
+        } else {
+          // 领取优惠券失败处理
+        }
+      }
+    });
+  },
+
+  /**
+   * 点击当前所领取的优惠券
+   */
+  getCouponGoods: function (e) {
+    const that = this,
+        coupon_info = this.data.couponInfo,
+        new_coupon_info = coupon_info,
+        coupon_id = e.currentTarget.id;
+    // 遍历coupon_info
+    for (let i = 0;i < coupon_info.length; i++){
+      // 找到当前点击的那条数据
+      if (coupon_info[i]["coupon_id"] == coupon_id) {
+        // 若当前优惠券没有商品列表则向后台请求商品列表
+        if (!coupon_info[i].hasOwnProperty("goods_list")) {
+          http({
+            url: "GetCouponGoods",
+            // 传入当前优惠券的id
+            data: {"coupon_id": coupon_id},
+            func: function(data){
+              // 将返回的商品列表放入new_coupon_info
+              new_coupon_info[i]["goods_list"] = data["goods_list"];
+              // 将当前数据的show_list取反
+              new_coupon_info[i]["show_list"] = !new_coupon_info[i]["show_list"];
+              // 重新设置couponInfo数据
+              that.setData({
+                couponInfo: new_coupon_info
+              })
+            }
+          });
+        } else {
+          // 将当前数据的show_list取反
+          new_coupon_info[i]["show_list"] = !new_coupon_info[i]["show_list"];
+          // 重新设置couponInfo数据
+          that.setData({
+            couponInfo: new_coupon_info
+          })
+        }
+      }
+    }
+},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
