e8b52cd94cb9a26a2161382ca95401120cf0859a
James.wang <james.wang@cdskysoft.com>
2017-09-13 14:15:30 +0800

    renew readme.

--- utils/util.js
+++ utils/util.js
@@ -1,22 +1,4 @@
 /******************************存放共通方法*********************************/
-function formatTime(date) {
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-
-
-    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-function formatNumber(n) {
-    n = n.toString();
-    return n[1] ? n : '0' + n
-}
-
 /**
  * 接口请求方法：wx.request发起的是https请求,一个微信小程序，最高并发数10个
  * @param options--调用该方法时传入的对象
@@ -44,6 +26,5 @@ function http(options) {
 
 //暴露封装的方法 
 module.exports = {
-    formatTime: formatTime,
     http                              //全局通用请求数据方法
 };
